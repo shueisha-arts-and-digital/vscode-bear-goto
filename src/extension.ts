52886166f7cc273f8c353925807af0bd26a035f7
Yuki Adachi <yuki777@gmail.com>
2021-09-01 23:36:01 +0900

    `yo code`で作り直す

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,30 @@
-"use strict";
-import * as vscode from "vscode";
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
 import PeekFileDefinitionProvider from "./PeekFileDefinitionProvider";
 
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  const configParams = vscode.workspace.getConfiguration("vscode-bear-goto");
-  const supportedLanguages = configParams.get("supportedLanguages") as Array<string>;
-  const targetFileExtensions = configParams.get("targetFileExtensions") as Array<string>;
-  const resourceAppPaths = configParams.get("resourceAppPaths") as Array<string>;
-  const resourcePagePaths = configParams.get("resourcePagePaths") as Array<string>;
+	const configParams = vscode.workspace.getConfiguration("vscode-bear-goto");
+	const supportedLanguages = configParams.get("supportedLanguages") as Array<string>;
+	const targetFileExtensions = configParams.get("targetFileExtensions") as Array<string>;
+	const resourceAppPaths = configParams.get("resourceAppPaths") as Array<string>;
+	const resourcePagePaths = configParams.get("resourcePagePaths") as Array<string>;
 
-  context.subscriptions.push(
-    vscode.languages.registerDefinitionProvider(
-      supportedLanguages,
-      new PeekFileDefinitionProvider(targetFileExtensions, resourceAppPaths, resourcePagePaths)
-    )
-  );
+	context.subscriptions.push(
+		vscode.languages.registerDefinitionProvider(
+			supportedLanguages,
+			new PeekFileDefinitionProvider(targetFileExtensions, resourceAppPaths, resourcePagePaths)
+		)
+	);
 
-  context.subscriptions.push(
-    vscode.languages.setLanguageConfiguration(
-      "php",
-      { wordPattern: /(get|post|put|delete)?\(?['"]([^'"]*?)['"]/ })
-  );
+	context.subscriptions.push(
+		vscode.languages.setLanguageConfiguration(
+			"php",
+			{ wordPattern: /(get|post|put|delete)?\(?['"]([^'"]*?)['"]/ })
+	);
 }
 
-export function deactivate() {
-}
+// this method is called when your extension is deactivated
+export function deactivate() { }
