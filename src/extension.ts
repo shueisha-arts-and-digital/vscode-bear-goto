7f612d694c7388980da69d0c8ee474d333389da4
Yuki Adachi <yuki777@gmail.com>
2021-09-01 23:39:26 +0900

    Fix cs

--- src/extension.ts
+++ src/extension.ts
@@ -6,24 +6,24 @@ import PeekFileDefinitionProvider from "./PeekFileDefinitionProvider";
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	const configParams = vscode.workspace.getConfiguration("vscode-bear-goto");
-	const supportedLanguages = configParams.get("supportedLanguages") as Array<string>;
-	const targetFileExtensions = configParams.get("targetFileExtensions") as Array<string>;
-	const resourceAppPaths = configParams.get("resourceAppPaths") as Array<string>;
-	const resourcePagePaths = configParams.get("resourcePagePaths") as Array<string>;
+  const configParams = vscode.workspace.getConfiguration("vscode-bear-goto");
+  const supportedLanguages = configParams.get("supportedLanguages") as Array<string>;
+  const targetFileExtensions = configParams.get("targetFileExtensions") as Array<string>;
+  const resourceAppPaths = configParams.get("resourceAppPaths") as Array<string>;
+  const resourcePagePaths = configParams.get("resourcePagePaths") as Array<string>;
 
-	context.subscriptions.push(
-		vscode.languages.registerDefinitionProvider(
-			supportedLanguages,
-			new PeekFileDefinitionProvider(targetFileExtensions, resourceAppPaths, resourcePagePaths)
-		)
-	);
+  context.subscriptions.push(
+    vscode.languages.registerDefinitionProvider(
+      supportedLanguages,
+      new PeekFileDefinitionProvider(targetFileExtensions, resourceAppPaths, resourcePagePaths)
+    )
+  );
 
-	context.subscriptions.push(
-		vscode.languages.setLanguageConfiguration(
-			"php",
-			{ wordPattern: /(get|post|put|delete)?\(?['"]([^'"]*?)['"]/ })
-	);
+  context.subscriptions.push(
+    vscode.languages.setLanguageConfiguration(
+      "php",
+      { wordPattern: /(get|post|put|delete)?\(?['"]([^'"]*?)['"]/ })
+  );
 }
 
 // this method is called when your extension is deactivated
